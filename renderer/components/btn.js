d6573388e62d22722da1057b7155277928c04954
Bayangmbe Mounmo <bayangp0@gmail.com>
2024-02-08 07:18:01 +0100

    add: check comp element

--- renderer/components/btn.js
+++ renderer/components/btn.js
@@ -2,8 +2,8 @@ import Inference from "./svg/inference"
 import Check from "./svg/check"
 import Usage from "./svg/usage"
 
-export default function Btn({ text, handleClick, enabled}) {
-    if(enabled) console.log(text, enabled)
+export default function Btn({ text, handleClick, enabled, extra}) {
+
     if (enabled) { 
         return (
             <div className='bg-[#E1C9DD] flex items-center mx-3 mt-[1px] rounded py-[5px] px-2 text-[#6C5468] hover:cursor-pointer transition duration-300'>
@@ -15,11 +15,12 @@ export default function Btn({ text, handleClick, enabled}) {
             )
     } else { 
         return (
-            <div onClick={handleClick} className='flex items-center mx-3 mt-[1px] rounded py-[5px] px-2 text-[#6C5468] hover:cursor-pointer hover:bg-[#E1C9DD]'>
+            <div onClick={handleClick} className='flex items-center mx-3 mt-[1px] rounded py-[5px] px-2 text-[#6C5468] hover:cursor-pointer hover:bg-[#E1C9DD] relative transi duration-500'>
                 { text === "Check" && <Check /> }
                 { text === "Inference" && <Inference /> }
-                { text === "Usage" && <Usage /> }
+                { text === "GPU Usage" && <Usage /> }
                 <span className='text-[#45363D] text-sm font-medium ml-2'>{text}</span>
+                { extra && <span className='absolute right-0 bg-[#E1C9DD] rounded-full font-semibold text-[#086AE3] px-[8px] py-1 text-xs'>{extra}%</span> }
             </div>
             )
     }
--- renderer/pages/home.js
+++ renderer/pages/home.js
@@ -10,21 +10,22 @@ if(typeof window !== "undefined" && window.process && window.process.type === "r
 export default function HomePage() {
   const [message, setMessage] = useState('No message found')
   const [tab, setTab] = useState('Check')
+  const [checking, setChecking] = useState(false)
 
-  useEffect(() => {
+  // useEffect(() => {
     
-    const listener = async (event, arg) => {
-      ipcRenderer.send('message', 'Hello from renderer')
-      ipcRenderer.on('message-reply', (event, arg) => {
-        console.log("DEBUG: ipcRenderer <listener>")
-        setMessage(arg)
-      })
-    }
+  //   const listener = async (event, arg) => {
+  //     ipcRenderer.send('message', 'Hello from renderer')
+  //     ipcRenderer.on('message-reply', (event, arg) => {
+  //       console.log("DEBUG: ipcRenderer <listener>")
+  //       setMessage(arg)
+  //     })
+  //   }
 
-    listener()
+  //   listener()
 
-    return () => { ipcRenderer.removeAllListeners('message') }
-  }, [])
+  //   return () => { ipcRenderer.removeAllListeners('message') }
+  // }, [])
 
   // when the user clicks on the button, the tab state changes
   const handleClick = (e) => {
@@ -43,19 +44,92 @@ export default function HomePage() {
         {/* left */}
         <div className='flex flex-col h-full w-[250px] bg-[#EED6EA]'>
           <div className=' bg-[#E1C9DD] flex mx-3 mt-7 rounded py-[2px] px-2 text-[#6C5468]'>@IP</div>
-
-          <div className='flex-1 pt-3'>
+          <div className='flex-1 pt-3 relative'>
             <Btn text="Check" handleClick={handleClick} enabled={"Check" === tab}/>
             <Btn text="Inference" handleClick={handleClick} enabled={"Inference" === tab} />
-            <Btn text="Usage" handleClick={handleClick} enabled={"Usage" === tab} />
+            <div className='absolute w-full bottom-3'>
+              <Btn text="GPU Usage" handleClick={() => {}} enabled={"" === tab} extra={45} />
+            </div>
           </div>
-
         </div>
 
         {/* center or right */}
-        <div className='flex w-full'>
-          right
-        </div>
+        {
+          tab === 'Check' &&
+          <div className='flex flex-col w-[750px]'>
+            <div className='flex items-center pl-7 h-[100px] text-3xl font-medium w-full relative'>
+              Check your hardware
+              <p className='text-lg font-normal absolute bottom-0'>Your GPU can support inference</p>
+            </div>
+            <div className='flex-1 flex flex-col '>
+              <div className='flex-1 flex justify-center gap-10'>
+                <div className=' h-[90%] my-auto w-[40%] flex items-center justify-center relative'>
+                  <div className=' flex justify-center gap-3 w-[300px] h-[300px] absolute  animate-pulse'>
+                    {
+                      Array.from({length: 10}, () => Math.floor(Math.random() * 10)).map((num, i) => (
+                        <div className=' bg-black w-[10px]  rounded-full h-[300px]'></div>
+                      ))
+                    } 
+                  </div>
+                  <div className=' flex-col flex items-center justify-center gap-3 w-[300px] h-[300px] absolute animate-pulse'>
+                    {
+                      Array.from({length: 10}, () => Math.floor(Math.random() * 10)).map((num, i) => (
+                        <div className=' bg-black h-[10px]  rounded-full w-[300px]'></div>
+                      ))
+                    } 
+                  </div>
+                  <div className=' flex-col flex items-center bg-black rounded-xl justify-center w-[250px] h-[250px] absolute'>
+                    <div className=' flex-col flex items-center bg-black border-[10px] border-white rounded-md justify-center w-[230px] h-[230px] absolute'>
+                    {
+                      checking ?
+                      <p className='text-[30px] text-white '>checking</p>
+                      :
+                      <p className='text-[70px] text-white '>GPU</p>
+                    }
+                    </div>
+                  </div>
+                </div>
+                <div className=' h-[90%] my-auto'></div>
+                <div className=' h-[90%] my-auto flex flex-col justify-center'>
+
+                  <div className=' border border-[#00000017] h-[80px] w-[150px] shadow-xl shadow-[#00000007] rounded-xl flex justify-center pt-2 relative'>
+                    <div className=' text-2xl font-medium'>24 GB </div>
+                    <div className=' absolute bottom-2 flex'>
+                      <span className='text-[#45363D]'>RAM</span>
+                    </div>
+                  </div>
+
+                  <div className=' border border-[#00000017] h-[80px] w-[150px] shadow-xl shadow-[#00000007] rounded-xl flex justify-center pt-2 relative mt-3'>
+                    <div className=' text-2xl font-medium'>6 GB </div>
+                    <div className=' absolute bottom-2 flex'>
+                      <span className='text-[#45363D]'>GPU</span>
+                    </div>
+                  </div>
+
+                  <div className=' border border-[#00000017] h-[80px] w-[150px] shadow-xl shadow-[#00000007] rounded-xl flex justify-center pt-2 relative mt-3'>
+                    <div className=' text-2xl font-medium'>32 Mbps </div>
+                    <div className=' absolute bottom-2 flex'>
+                      <span className='text-[#45363D]'>SPEED</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className=' h-[100px]'></div>
+            </div>
+          </div>
+        }
+        {
+          tab === 'Inference' &&
+          <div className='flex flex-col w-[750px]'>
+            
+          </div>
+        }
+        {
+          tab === 'Usage' &&
+          <div className='flex flex-col w-[750px]'>
+            
+          </div>
+        }
         
       </div>
     </main>
